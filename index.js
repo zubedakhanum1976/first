2497263a9bb380cfe2f8d55a3713d914b8ee6595
Zubeda Khanum <zubedakhanum1976@gmail.com>
2026-06-01 13:35:39 +0100

    set

--- index.js
+++ index.js
@@ -6,6 +6,8 @@ app.get('/', (req, res) => {
   res.send({ message: process.env.MESSAGE });
 });
 
-app.listen(3000, () => {
-  console.log('Server is running on port 3000');
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log('Server is running on port ' + port);
 });
+
